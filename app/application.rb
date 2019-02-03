19479807a75699f62f1a6c4bc18686331d696de4
Elias Ventura <eliasventura23@gmail.com>
2019-02-03 04:18:50 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -25,10 +25,12 @@ class Application
 
     elsif req.path.match(/add/)
       search_term = req.params["item"]
+      if @@items.include?(search_term)
       @@cart << search_term
-      resp.write handle_search(search_term)
-
-
+      resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
+      end
     else
      resp.write "Path Not Found"
    end
