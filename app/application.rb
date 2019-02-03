589ca3a6c87a8198773d0b5398974c06e4979fb2
Elias Ventura <eliasventura23@gmail.com>
2019-02-03 03:53:20 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -14,7 +14,7 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
-    elsif req.path.match(/carts/)
+    elsif req.path.match(/cart/)
       if @@carts.empty?
       resp.write "Your cart is empty"
       elsif req.path.match(/carts/)
