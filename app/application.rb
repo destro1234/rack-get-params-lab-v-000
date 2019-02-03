703476a1a5db6cb3b278e624d4a3282b2eb20460
Elias Ventura <eliasventura23@gmail.com>
2019-02-03 04:04:35 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,7 +3,15 @@ class Application
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new
-    resp.finish
+
+    if req.path.match(/items/)
+      @@items.each do |item|
+      resp.write "#{item}\n"
+    end
+    elsif req.path.match(/search/)
+      search_term = req.params["q"]
+      resp.write handle_search(search_term)
+      resp.finish
   end
 
 end
